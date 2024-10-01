d46d3baa1ca2b8d2dc11915c819ef0055e8284bc
Chandrasekharrebba99 <chandui17cy028@gmail.com>
2024-10-01 14:25:03 +0530

    redux fixing

--- src/views/Outlet.jsx
+++ src/views/Outlet.jsx
@@ -6,16 +6,27 @@ import SignupOffer from '../components/SignupOffer';
 import FilterDrawer from '../components/filter';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchProducts } from '../redux/slices/productSlice';
+import { fetchProducts } from '../redux/slices/productslice';
 
 const Outlet = () => {
+  // const dispatch = useDispatch();
+  // const { items, status, error } = useSelector((state) => state.products);
+
+  // useEffect(() => {
+  //   if (status === 'idle') {
+  //     dispatch(fetchProducts()); // Dispatch the thunk to fetch products
+  //   }
+  //   console.log(items,"Items")
+  // }, [dispatch, status]);
+
+  
   return <div>
     <SaleOffer/>
     <div>
       <FilterDrawer/>
       <hr/>
       <div>
-        
+       
       </div>
     </div>
 
