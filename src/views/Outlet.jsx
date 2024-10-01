62ea8274931ae6a28a5feb3ddbf271cd779a4f1e
Chandrasekharrebba99 <chandui17cy028@gmail.com>
2024-10-01 15:38:41 +0530

    product show case

--- src/views/Outlet.jsx
+++ src/views/Outlet.jsx
@@ -7,17 +7,19 @@ import FilterDrawer from '../components/filter';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from '../redux/slices/productslice';
+import {useEffect} from 'react'
+
 
 const Outlet = () => {
-  // const dispatch = useDispatch();
-  // const { items, status, error } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  const { items, status, error } = useSelector((state) => state.products);
 
-  // useEffect(() => {
-  //   if (status === 'idle') {
-  //     dispatch(fetchProducts()); // Dispatch the thunk to fetch products
-  //   }
-  //   console.log(items,"Items")
-  // }, [dispatch, status]);
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchProducts()); // Dispatch the thunk to fetch products
+    }
+    console.log(items,"Items")
+  }, [dispatch, status]);
 
   
   return <div>
@@ -25,7 +27,19 @@ const Outlet = () => {
     <div>
       <FilterDrawer/>
       <hr/>
-      <div>
+      <div className='all-products-maincont'>
+        {items.map((item)=>{
+          return(
+            <div className='item-img-container'>
+              <img src={item.image} className='itemimg'/>
+              <p>${item.price}</p>
+              <p>${item.price+item.price*0.3}:-30% original price</p>
+              <p>{item.title.slice(0,10)}</p>
+              <p>{item.category}</p>
+              <p>{item.rating.rate}people:<span>{item.rating.count}</span></p>
+            </div>
+          )
+        })}
        
       </div>
     </div>
