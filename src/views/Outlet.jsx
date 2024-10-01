b765692414ac815ebbbecb95217c29a9cc33dd6b
Chandrasekharrebba99 <chandui17cy028@gmail.com>
2024-10-01 18:33:33 +0530

    add to cart

--- src/views/Outlet.jsx
+++ src/views/Outlet.jsx
@@ -25,23 +25,37 @@ const Outlet = () => {
   return <div>
     <SaleOffer/>
     <div>
-      <FilterDrawer/>
+      <div className='FilterContent'>
+        <FilterDrawer/>
+      </div>
       <hr/>
+      <div className='all-products'>
       <div className='all-products-maincont'>
         {items.map((item)=>{
           return(
             <div className='item-img-container'>
               <img src={item.image} className='itemimg'/>
-              <p>${item.price}</p>
-              <p>${item.price+item.price*0.3}:-30% original price</p>
+              <div className='product-content'>
+              <p className='price-red'>${item.price}</p>
+              <div className='make-flex'>  <p className='original-price'>${Math.floor(item.price+item.price*0.3)}</p>
+              
+              <span>: -30%  Original price</span>
+              </div>
+              <br/>
+              <span className='item-cat'>{item.category}</span>
               <p>{item.title.slice(0,10)}</p>
-              <p>{item.category}</p>
-              <p>{item.rating.rate}people:<span>{item.rating.count}</span></p>
+              <p>{Math.floor(Math.random()*10)+1} colors</p>
+              <button className='addToCartBtn'>Add to Cart</button>
+              {/* <p>{item.rating.rate}people:<span>{item.rating.count}</span></p> */}
+              </div>
+        
             </div>
           )
         })}
        
       </div>
+      </div>
+
     </div>
 
   </div>;
